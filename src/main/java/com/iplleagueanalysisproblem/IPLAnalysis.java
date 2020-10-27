fd770294cc970b0345385f0f627d565a453ebbfa
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-27 16:27:41 +0530

    Added : Functionality to load CSV files

--- src/main/java/com/iplleagueanalysisproblem/IPLAnalysis.java
+++ src/main/java/com/iplleagueanalysisproblem/IPLAnalysis.java
@@ -1,6 +1,34 @@
 package com.iplleagueanalysisproblem;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.CSVBuilder.CSVBuilderException;
+import com.CSVBuilder.CSVBuilderFactory;
+import com.CSVBuilder.ICSVBuilder;
+
 public class IPLAnalysis {
+	List<CSVRuns> runsCSVList = null;
+	List<CSVWickets> wicketsCSVList = null;
+	
+	public static <k> List<k> loadCSVData(String filePath, Class<k> csvClass) throws CSVBuilderException, IOException {
+		Reader reader = Files.newBufferedReader(Paths.get(filePath)); // no such file exception
+		ICSVBuilder<k> csvBuilder = CSVBuilderFactory.createCSVBuilder();
+		return csvBuilder.getCSVFileList(reader, csvClass);
+	}
+	
+	public int loadRunsCSV(String filePath) throws CSVBuilderException, IOException {
+		runsCSVList = loadCSVData(filePath, CSVRuns.class);
+		return runsCSVList.size();
+	}
+	
+	public int loadWicketsCSV(String filePath) throws CSVBuilderException, IOException {
+		wicketsCSVList = loadCSVData(filePath, CSVWickets.class);
+		return wicketsCSVList.size();
+	}	
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to IPL League analysis Problem");
--- src/test/java/com/iplleagueanalysisproblem/IPLAnalysisTest.java
+++ src/test/java/com/iplleagueanalysisproblem/IPLAnalysisTest.java
@@ -5,5 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class IPLAnalysisTest {
-	
+	private static String RUNS_FILE_PATH = "C:\\Users\\abc\\eclipse-workspace\\com.iplleagueanalysisproblem\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	private static String WICKET_FILE_PATH = "C:\\Users\\abc\\eclipse-workspace\\com.iplleagueanalysisproblem\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
+
+	@Test
+	void givenRunsFilePath_shouldReturn_NumberOfRecords() {
+		IPLAnalysis iplAnalysis = new IPLAnalysis();
+		int numOfRecords = 0;
+		try {
+			numOfRecords = iplAnalysis.loadRunsCSV(RUNS_FILE_PATH);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		assertEquals(101, numOfRecords);
+	}
+
+	@Test
+	void givenWicketsFilePath_shouldReturn_NumberOfRecords() {
+		IPLAnalysis iplAnalysis = new IPLAnalysis();
+		int numOfRecords = 0;
+		try {
+			numOfRecords = iplAnalysis.loadWicketsCSV(WICKET_FILE_PATH);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		assertEquals(99, numOfRecords);
+	}
 }
