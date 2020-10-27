ed5bb9a486b3cc3d1b5c5e7e7a6708c069ceb5e6
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-27 17:00:37 +0530

    Added : Functionality to get top batting average from runs CSV

--- src/main/java/com/iplleagueanalysisproblem/IPLAnalysis.java
+++ src/main/java/com/iplleagueanalysisproblem/IPLAnalysis.java
@@ -13,23 +13,63 @@ import com.CSVBuilder.ICSVBuilder;
 public class IPLAnalysis {
 	List<CSVRuns> runsCSVList = null;
 	List<CSVWickets> wicketsCSVList = null;
-	
-	public static <k> List<k> loadCSVData(String filePath, Class<k> csvClass) throws CSVBuilderException, IOException {
+
+	/**
+	 * Loads csv file data and returns list of POJO objects
+	 * 
+	 * @param <k>
+	 * @param filePath
+	 * @param csvClass
+	 * @return
+	 * @throws CSVBuilderException
+	 * @throws IOException
+	 */
+	private static <k> List<k> loadCSVData(String filePath, Class<k> csvClass) throws CSVBuilderException, IOException {
 		Reader reader = Files.newBufferedReader(Paths.get(filePath)); // no such file exception
 		ICSVBuilder<k> csvBuilder = CSVBuilderFactory.createCSVBuilder();
 		return csvBuilder.getCSVFileList(reader, csvClass);
 	}
-	
+
+	/**
+	 * Loads csv file data and returns size of list of POJO objects
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws CSVBuilderException
+	 * @throws IOException
+	 */
 	public int loadRunsCSV(String filePath) throws CSVBuilderException, IOException {
 		runsCSVList = loadCSVData(filePath, CSVRuns.class);
 		return runsCSVList.size();
 	}
-	
+
+	/**
+	 * Loads csv file data and returns size of list of POJO objects
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws CSVBuilderException
+	 * @throws IOException
+	 */
 	public int loadWicketsCSV(String filePath) throws CSVBuilderException, IOException {
 		wicketsCSVList = loadCSVData(filePath, CSVWickets.class);
 		return wicketsCSVList.size();
-	}	
-	
+	}
+
+	/**
+	 * UC 1 : returns top batting average
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws CSVBuilderException
+	 * @throws IOException
+	 */
+	public double getTopBattingAvg(String filePath) throws CSVBuilderException, IOException {
+		loadRunsCSV(filePath);
+		double max = runsCSVList.stream().map(entry -> entry.average).max(Double::compare).get();
+		return max;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to IPL League analysis Problem");
 	}
--- src/test/java/com/iplleagueanalysisproblem/IPLAnalysisTest.java
+++ src/test/java/com/iplleagueanalysisproblem/IPLAnalysisTest.java
@@ -2,15 +2,28 @@ package com.iplleagueanalysisproblem;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.CSVBuilder.CSVBuilderException;
+
 class IPLAnalysisTest {
+	private static IPLAnalysis iplAnalysis;
 	private static String RUNS_FILE_PATH = "C:\\Users\\abc\\eclipse-workspace\\com.iplleagueanalysisproblem\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
 	private static String WICKET_FILE_PATH = "C:\\Users\\abc\\eclipse-workspace\\com.iplleagueanalysisproblem\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
 
+	@BeforeAll
+	static void setUp() {
+		iplAnalysis = new IPLAnalysis();
+	}
+
+	/**
+	 * checking number of records of runs CSV file
+	 */
 	@Test
 	void givenRunsFilePath_shouldReturn_NumberOfRecords() {
-		IPLAnalysis iplAnalysis = new IPLAnalysis();
 		int numOfRecords = 0;
 		try {
 			numOfRecords = iplAnalysis.loadRunsCSV(RUNS_FILE_PATH);
@@ -20,9 +33,11 @@ class IPLAnalysisTest {
 		assertEquals(101, numOfRecords);
 	}
 
+	/**
+	 * checking number of records of wickets CSV file
+	 */
 	@Test
 	void givenWicketsFilePath_shouldReturn_NumberOfRecords() {
-		IPLAnalysis iplAnalysis = new IPLAnalysis();
 		int numOfRecords = 0;
 		try {
 			numOfRecords = iplAnalysis.loadWicketsCSV(WICKET_FILE_PATH);
@@ -30,5 +45,17 @@ class IPLAnalysisTest {
 			exception.printStackTrace();
 		}
 		assertEquals(99, numOfRecords);
+	}
+
+	/**
+	 * UC 1 : checking top batting average
+	 * 
+	 * @throws CSVBuilderException
+	 * @throws IOException
+	 */
+	@Test
+	void givenRunsFilePath_shouldReturn_topBattingAvg() throws CSVBuilderException, IOException {
+		double maxBattingAvg = iplAnalysis.getTopBattingAvg(RUNS_FILE_PATH);
+		assertEquals(83.2, maxBattingAvg);
 	}
 }
