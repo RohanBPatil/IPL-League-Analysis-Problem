d74e64d44e5aac6fb8734fbdfa96780084d18472
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-27 16:22:47 +0530

    Added : Welcome to IPL league analysis problem

---
+++ src/main/java/com/iplleagueanalysisproblem/IPLAnalysis.java
@@ -0,0 +1,8 @@
+package com.iplleagueanalysisproblem;
+
+public class IPLAnalysis {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to IPL League analysis Problem");
+	}
+}
---
+++ src/test/java/com/iplleagueanalysisproblem/IPLAnalysisTest.java
@@ -0,0 +1,9 @@
+package com.iplleagueanalysisproblem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IPLAnalysisTest {
+	
+}
